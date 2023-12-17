8a491d3c609dad9a9db786030fdb58bcf859a926
lencho <lenchoajema@gmail.com>
2023-12-17 09:41:44 -0800

    AirBnB v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,18 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
-
-apt-get update
-apt-get install -y nginx
-
+# sets up web servers for the deployment of web_static.
+apt update -y
+apt install nginx -y
+ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "ALX Softeware Engineering" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://github.com/lencho;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
